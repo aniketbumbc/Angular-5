a145243d9041a1d5e7f0cff74a0d9e9cd1991da7
aniket umbc <aniket.umbc@gmail.com>
2017-11-05 07:20:43 +0530

    Adding Routerlink and routes to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponentComponent } from './home-component/home-component.component';
 import { AboutComponentComponent } from './about-component/about-component.component';
 import { ProjectComponentComponent } from './project-component/project-component.component';
 import { ContactComponentComponent } from './contact-component/contact-component.component';
 import { ExpComponentComponent } from './exp-component/exp-component.component';
+
+const appRoutes: Routes = [
+{path: '', component: HomeComponentComponent},
+{path: 'About', component: AboutComponentComponent},
+{path: 'Project', component: ProjectComponentComponent},
+{path: 'Contact', component: ContactComponentComponent},
+{path: 'Exp', component: ExpComponentComponent},
+];
+
 
 
 @NgModule({
@@ -19,7 +29,8 @@ import { ExpComponentComponent } from './exp-component/exp-component.component';
     ExpComponentComponent],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
