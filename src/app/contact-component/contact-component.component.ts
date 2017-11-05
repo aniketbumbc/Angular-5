22e142a69f277bb358503fdb58dbfef74fd4c8d1
aniket umbc <aniket.umbc@gmail.com>
2017-11-05 17:19:08 +0530

    Template Driven End

--- src/app/contact-component/contact-component.component.ts
+++ src/app/contact-component/contact-component.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component,ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -7,10 +7,26 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./contact-component.component.css']
 })
 export class ContactComponentComponent {
-
+@ViewChild('f') EmailForm:NgForm;
+  genders=['Male','Female'];
+  user={
+    Email:'',
+    Password:'',
+    City:'',
+    Address:'',
+    Zipcode:'',
+    State:''
+  };
   constructor() { }
 
- onSubmit(form:NgForm){
-  console.log(form);
+ onSubmit(){
+this.user.Email=this.EmailForm.value.email;
+this.user.Password=this.EmailForm.value.password;
+this.user.City=this.EmailForm.value.city;
+this.user.Address=this.EmailForm.value.address;
+this.user.Zipcode=this.EmailForm.value.zipcode;
+this.user.State=this.EmailForm.value.state;
+//console.log(this.EmailForm);
+
  }
 }
